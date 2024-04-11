74f58a2602633f41aa6721c825b4c5059e485174
Vinod Kumar N <vinodkumar.nelanakula@carrier.com>
2024-04-11 23:00:40 +0530

    Added tailwind css to project

--- src/componants/Body.tsx
+++ src/componants/Body.tsx
@@ -1,7 +1,7 @@
 import RestaurantCard from './RestaurantCard';
 import React, { useState, useEffect } from 'react';
 import Shimmer from './shimmer';
-import { SWIGGY_API } from '../utils/constants';
+import { CORSS_PROXY, SWIGGY_API } from '../utils/constants';
 import { fetchData } from '../utils/dataFetch';
 import { Link } from 'react-router-dom';
 import useRestaurantsData from '../hooks/useRestaurantsData';
@@ -20,19 +20,30 @@ const Body = () => {
     setFilteredData(resList);
   })
 
+  const fetchData = async() => {
+    const d = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.4485835&lng=78.39080349999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING')
+    console.log('d', d)
+    const j = d.json()
+    console.log('j', j)
+  }
+  useEffect(() => {
+    fetchData()
+  }, [])
+  
   if (!internetStatus) {
     return <h1>Looks like you are offline, check you connection!</h1>
   }
 
   return !restaurant?.length ? <Shimmer /> : (
     <div className='body'>
-      <div className='filter'>
-        <div className='search'>
+      <div className='flex items-center'>
+        <div className='m-4 p-4'>
           <input type='text'
-            className='search-box'
+            className='border border-solid border-black'
             value={searchText}
             onChange={(value) => { setSearchText(value.target.value) }} />
           <button
+            className='px-2 py-2 bg-green-100 ml-2 rounded-lg'
             onClick={() => {
               console.log('searchText', searchText)
               setFilteredData(restaurant.filter(res => 
@@ -40,17 +51,23 @@ const Body = () => {
               ))
             }}>Search</button>
         </div>
-        <button className="filter-btn" onClick={() => {
-          const filteredList = restaurant.filter(
-            (res) => res?.info.avgRating > 4
-          );
-          setFilteredData(filteredList);
-        }}
-        >Top Rated Restaurant</button>
+        <div className='m-4 p-4'>
+          <button
+            className="px-2 py-2 bg-gray-50 rounded-lg"
+            onClick={() => {
+              const filteredList = restaurant.filter(
+                (res) => res?.info.avgRating > 4
+              );
+              setFilteredData(filteredList);
+            }}
+          >Top Rated Restaurant</button>
+        </div>
       </div>
-      <div className='res-container'>
+      <div className='flex flex-wrap'>
         {filteredData && filteredData.map((res) => (
-          <Link to={'/restaurants/'+res?.info.id} key={res?.info.id}><RestaurantCard resData={res?.info} /></Link>
+          <Link to={'/restaurants/' + res?.info.id} key={res?.info.id}>
+            <RestaurantCard resData={res?.info} />
+          </Link>
         ))}
       </div>
 
--- src/componants/Header.tsx
+++ src/componants/Header.tsx
@@ -15,19 +15,22 @@ const Header = () => {
    */
 
   return (
-    <div className='header' >
+    <div className='flex justify-between shadow-lg
+      bg-yellow-100
+      sm:bg-pink-100
+      lg:bg-green-100' >
       <div className='logo-container' >
-        <img className='logo' src={LOGO_URL} />
+        <img className='w-24' src={LOGO_URL} />
       </div>
-      < div className='nav' >
-        <ul>
-          <li>Online Status: {internetStatus ? '✅': '🔴'}</li>
-          <li><Link to='/'>Home</Link></li>
-          <li> <Link to='/grocery'>Grocery</Link></li>
-          <li> <Link to='/about'>About Us</Link></li>
-          <li> <Link to='/contact'>Contact Us</Link></li>
-          <li> Cart </li>
-          <button className='login' onClick={() => { setBtnName(!btnName); console.log('btnName', btnName)}}>{btnName?'Log In':'Log Out'}</button>
+      < div className='flex items-center' >
+        <ul className='flex p-4 m-4'>
+          <li className="px-4">Online Status: {internetStatus ? '✅': '🔴'}</li>
+          <li className="px-4"><Link to='/'>Home</Link></li>
+          <li className="px-4"> <Link to='/grocery'>Grocery</Link></li>
+          <li className="px-4"> <Link to='/about'>About Us</Link></li>
+          <li className="px-4"> <Link to='/contact'>Contact Us</Link></li>
+          <li className="px-4"> Cart </li>
+          <button className='px-4' onClick={() => { setBtnName(!btnName); console.log('btnName', btnName)}}>{btnName?'Log In':'Log Out'}</button>
         </ul>
       </div>
     </div>
